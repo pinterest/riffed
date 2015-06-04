f0f8c507efdeeaf2973a0c69454f8f2a3f059814
Steve Cohen <steve@pinterest.com>
2015-06-04 14:27:57 -0700

    Changed location of thrift repo to phabricator
    Reviewers: kelsey, moliver
    
    Reviewed By: moliver
    
    Differential Revision: https://phabricator.pinadmin.com/D29143

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Rift.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-        {:thrift, git: "git@github.pinadmin.com:jon/elixir-thrift.git", submodules: true},
+        {:thrift, git: "ssh://git@phabricator.pinadmin.com/diffusion/EXT/elixir-thrift.git", submodules: true},
         {:meck, github: "eproxus/meck", tag: "0.8.2", override: true},
         {:mock, github: "jjh42/mock"},
     ]
