fbf1b259e8fee598d5f98eca1fae28c5437aed17
Jon Parise <jon@pinterest.com>
2015-12-19 09:28:41 -0800

    Use the public GitHub version of elixir-thrift.

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule Riffed.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-        {:thrift, git: "git@github.com:pinterest/elixir-thrift.git", tag: "1.0.0", submodules: true},
+        {:thrift, github: "pinterest/elixir-thrift", tag: "1.0.0", submodules: true},
         {:meck, "~> 0.8.2"},
         {:mock, github: "jjh42/mock"},
         {:exlager, github: "khia/exlager"},
