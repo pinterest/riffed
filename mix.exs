ca083d52d4ecfad822432da3925f644bc748f260
Steve Cohen <steve@pinterest.com>
2016-01-04 09:45:44 -0800

    Bumped version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Riffed.Mixfile do
   def project do
     [app: :riffed,
      name: "Riffed",
-     version: "0.1.0",
+     version: "1.0.0",
      elixir: "~> 1.0",
      deps: deps,
      compilers: compilers(Mix.env),
