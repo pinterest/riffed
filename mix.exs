013824330156650080ff12eef175068178a15a96
Jon Parise <jon@pinterest.com>
2016-11-09 11:14:01 -0800

    Drop Lager and use Logger instead
    This simplifies our dependencies, and Logger -> Lager bridges exist for
    those applications that prefer Lager-based logging.

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Riffed.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:thrift, :exlager]]
+    [applications: [:logger, :thrift]]
   end
 
   def compilers(:test) do
@@ -43,7 +43,6 @@ defmodule Riffed.Mixfile do
         {:thrift, "~> 1.3"},
         {:meck, "~> 0.8.2", only: [:test, :dev]},
         {:mock, github: "jjh42/mock", only: [:test, :dev]},
-        {:exlager, github: "khia/exlager"},
         {:earmark, "~> 0.1", only: :dev},
         {:ex_doc, "~> 0.8", only: :dev},
         {:excoveralls, github: "parroty/excoveralls", tag: "v0.4.5", override: true, only: :test}
